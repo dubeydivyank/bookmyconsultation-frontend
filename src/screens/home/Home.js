ef3d2b36b52318e96390f34d83f8976df0927fe7
Divyank <dubeydivyank@gmail.com>
2022-05-13 17:11:37 +0530

    display doctors & apponintment tabs on home

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,7 +1,40 @@
 import Header from "../../common/header/Header";
+import React, { useState } from "react";
+import DoctorList from "../doctorList/DoctorList";
+import Appointment from "../appointment/Appointment";
+import { Tab, Tabs } from "@material-ui/core";
 
 const Home = () => {
-  return <Header />;
+  const [value, setValue] = useState(0);
+  const tabSwitchHandler = (event, value) => {
+    setValue(value);
+  };
+  return (
+    <div>
+      <Header />
+      <Tabs
+        variant="fullWidth"
+        indicatorColor="primary"
+        value={value}
+        onChange={tabSwitchHandler}
+      >
+        <Tab label="Doctors"></Tab>
+        <Tab label="Appointment"></Tab>
+      </Tabs>
+      {value === 0 && (
+        <DoctorList
+        // getUserAppointments={getUserAppointments}
+        // userAppointments={userAppointments}
+        />
+      )}
+      {value === 1 && (
+        <Appointment
+        // userAppointments={userAppointments}
+        // isLogin={isLogin}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Home;
